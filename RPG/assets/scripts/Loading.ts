b5ba19235a48913974bbc15b75dda32befbbfaf6
xzc <2430455821@qq.com>
2021-02-07 17:21:11 +0800

    加载界面

---
+++ RPG/assets/scripts/Loading.ts
@@ -0,0 +1,44 @@
+/*
+ * @Author: Xiong ZhiCheng 
+ * @Date: 2021-02-06 14:14:33 
+ * @Description: 加载
+ */
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export class Loading extends cc.Component {
+
+    @property({ displayName: "背景1", type: cc.Node })
+    private bg1: cc.Node = null;
+
+    @property({ displayName: "背景2", type: cc.Node })
+    private bg2: cc.Node = null;
+
+    private screenWidth: number = 0;
+
+    public onLoad(): void {
+        this.screenWidth = cc.Canvas.instance.node.width;
+        this.bg1.x = -this.screenWidth / 2;
+        this.bg2.x = this.bg1.width + this.bg1.x;
+    }
+
+    public update(dt: number): void {
+
+        this.bg1.x -= dt * 200;
+        this.bg2.x -= dt * 200;
+
+        this.checkBGPosition(dt);
+    }
+
+    private checkBGPosition(dt: number): void {
+        let bg1MinX = -(this.screenWidth / 2 + this.bg1.width);
+        if (this.bg1.x < bg1MinX) {
+            this.bg1.x = this.bg2.x + this.bg2.width;
+        }
+
+        let bg2MinX = -(this.screenWidth / 2 + this.bg2.width);
+        if (this.bg2.x < bg2MinX) {
+            this.bg2.x = this.bg2.x + this.bg2.width;
+        }
+    }
+}
