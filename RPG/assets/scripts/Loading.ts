ebcd2301dbf510bec70be9ae19622de702599fea
xzc <2430455821@qq.com>
2021-02-20 16:16:26 +0800

    添加加载完成进入main场景逻辑

--- RPG/assets/scripts/Loading.ts
+++ RPG/assets/scripts/Loading.ts
@@ -19,20 +19,43 @@ export class Loading extends cc.Component {
 
     private screenWidth: number = 0;
 
+    private loadComplete: boolean = false;
+
+    private sceneLoadComplect: boolean = false;
+
+    private progress: number = 0;
+
     public onLoad(): void {
         this.screenWidth = cc.Canvas.instance.node.width;
         this.bg1.x = -this.screenWidth / 2;
         this.bg2.x = this.bg1.width + this.bg1.x;
         this.progressBar.fillRange = 0;
+        this.loadComplete = false;
+        this.sceneLoadComplect = false;
+
+        cc.director.preloadScene("main", null, (error) => {
+            if (error) {
+                console.error("load main scene fail");
+                return;
+            }
+            this.sceneLoadComplect = true;
+        });
     }
 
     public update(dt: number): void {
         this.refreshProgress(dt);
         this.checkBGPosition(dt);
+
+        if (this.progress >= 1 && this.sceneLoadComplect && !this.loadComplete) {
+            this.loadComplete = true;
+            this.loadScene();
+        }
     }
 
     private refreshProgress(dt: number): void {
-        this.progressBar.fillRange += 0.003;
+        this.progress += 0.003;
+        this.progress = Math.min(this.progress, 1);
+        this.progressBar.fillRange = this.progress;
     }
 
     private checkBGPosition(dt: number): void {
@@ -49,5 +72,9 @@ export class Loading extends cc.Component {
         if (this.bg2.x < bg2MinX) {
             this.bg2.x = this.bg1.x + this.bg1.width;
         }
+    }
+
+    private loadScene(): void {
+        cc.director.loadScene("main");
     }
 }
